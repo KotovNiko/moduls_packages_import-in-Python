b506c3a71ea0b650a44c030ddcbd3f0fb4041899
Kotov Nikolay <kotov.1806@gmail.com>
2026-03-29 23:12:22 +0700

    Update main.py

--- homework/main.py
+++ homework/main.py
@@ -1,6 +1,6 @@
 from datetime import datetime
-from homework.application.salary import calculate_salary
-from homework.application.db.people import get_employees
+from application.salary import calculate_salary
+from application.db.people import get_employees
 
 if __name__ == '__main__':
     current_date = datetime.now().strftime("%d.%m.%Y %H:%M:%S")
